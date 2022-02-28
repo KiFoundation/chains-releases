3b82ab2cb0e1763c77930cd4dc7c45cdb1f6d3f7
Nicolas Nativel <nicolas@klub.ki>
2022-02-28 10:28:55 +0100

    Add osmosis v7 build flow

---
+++ osmosis/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+apk add git gcc build-base linux-headers
+wget https://github.com/CosmWasm/wasmvm/releases/download/v1.0.0-beta5/libwasmvm_muslc.a -O /lib/libwasmvm_muslc.a
+
+git clone https://github.com/osmosis-labs/osmosis.git
+cd osmosis
+git checkout v7.0.2
+BUILD_TAGS=muslc make build
