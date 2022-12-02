d3216b65b8affc40d2f82d79fc21f2dc9638c104
Nicolas Nativel <nicolas@klub.ki>
2022-12-02 08:53:03 +0100

    Add osmosis testnet build

---
+++ osmosis_testnet/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+apk add git gcc build-base linux-headers
+wget https://github.com/CosmWasm/wasmvm/releases/download/v1.1.1/libwasmvm_muslc.x86_64.a -O /lib/libwasmvm_muslc.a
+
+git clone https://github.com/osmosis-labs/osmosis.git
+cd osmosis
+git checkout v13.0.0-rc5-testnet2
+BUILD_TAGS=muslc LINK_STATICALLY=true make build
