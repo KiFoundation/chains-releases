22c60c68bb510088ae9e121cc18617f0a085a3d2
Nicolas Nativel <nicolas@klub.ki>
2022-12-02 08:52:36 +0100

    Update osmosis build

--- osmosis/build.sh
+++ osmosis/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 apk add git gcc build-base linux-headers
-wget https://github.com/CosmWasm/wasmvm/releases/download/v1.0.0/libwasmvm_muslc.x86_64.a -O /lib/libwasmvm_muslc.a
+wget https://github.com/CosmWasm/wasmvm/releases/download/v1.1.1/libwasmvm_muslc.x86_64.a -O /lib/libwasmvm_muslc.a
 
 git clone https://github.com/osmosis-labs/osmosis.git
 cd osmosis
-git checkout v10.0.0
+git checkout v13.0.0-rc4
 BUILD_TAGS=muslc LINK_STATICALLY=true make build
