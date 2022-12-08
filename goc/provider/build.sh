17b3eecc36c4e42deab0af70d53580315a174e4b
Nicolas Nativel <nicolas@klub.ki>
2022-12-08 09:35:48 +0100

    Fix version for goc provider

--- goc/provider/build.sh
+++ goc/provider/build.sh
@@ -4,6 +4,6 @@ apk add git gcc build-base linux-headers
 
 git clone https://github.com/cosmos/gaia.git
 cd gaia
-git checkout glnro/ics-sdk45
+git checkout 2627f224c78010c1884f7e8da3d9ce0d5b54a812
 
 BUILD_TAGS=muslc LDFLAGS='-linkmode=external -extldflags "-Wl,-z,muldefs -static"' make build
