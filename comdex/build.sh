8ba3a3e3cd6fcd5016e16782cc6a76c75ec33cf6
NATIVEL Nicolas <nicolas@klub.ki>
2023-02-03 09:08:27 +0100

    Update comdex to v8.1.0

--- comdex/build.sh
+++ comdex/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 apk add git gcc build-base linux-headers
-wget https://github.com/CosmWasm/wasmvm/releases/download/v1.0.0/libwasmvm_muslc.x86_64.a -O /lib/libwasmvm_muslc.a
+wget https://github.com/CosmWasm/wasmvm/releases/download/v1.1.1/libwasmvm_muslc.x86_64.a -O /lib/libwasmvm_muslc.a
 
 git clone https://github.com/comdex-official/comdex.git
 cd comdex
-git checkout v7.0.0
+git checkout v8.1.0
 
 BUILD_TAGS=muslc LINK_STATICALLY=true make build
