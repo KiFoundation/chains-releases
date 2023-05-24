e8a9b86f04de8c1a1fbacb345d681b4b025e3052
Nicolas Nativel <nicolas@klub.ki>
2023-05-24 08:42:28 +0200

    lum v1.4.0

--- lum/build.sh
+++ lum/build.sh
@@ -4,5 +4,5 @@ apk add git gcc build-base linux-headers
 
 git clone https://github.com/lum-network/chain.git
 cd chain
-git checkout v1.3.1
-BUILD_TAGS=muslc LINK_STATICALLY=true make build
+git checkout v1.4.0
+BUILD_TAGS=muslc LINK_STATICALLY=true make install
