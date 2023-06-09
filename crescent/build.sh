b247e05447cf0af7b38203a8ebb2b5f18c52cc36
Nicolas Nativel <nicolas@klub.ki>
2023-06-09 11:45:34 +0200

    crescent_v4.2.0

--- crescent/build.sh
+++ crescent/build.sh
@@ -4,5 +4,5 @@ apk add git gcc build-base linux-headers
 
 git clone https://github.com/crescent-network/crescent.git
 cd crescent
-git checkout v4.1.1
+git checkout v4.2.0
 BUILD_TAGS=muslc LDFLAGS='-linkmode=external -extldflags "-Wl,-z,muldefs -static"' make build
