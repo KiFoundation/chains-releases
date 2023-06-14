c62b9e3a9c0b77b12615d6c4dc4cc5e6b8c256ca
Nicolas Nativel <nicolas@klub.ki>
2023-06-14 08:35:34 +0200

    comedx v11.5.0

--- comdex/build.sh
+++ comdex/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 apk add git gcc build-base linux-headers
-wget https://github.com/CosmWasm/wasmvm/releases/download/v1.1.1/libwasmvm_muslc.x86_64.a -O /lib/libwasmvm_muslc.a
+wget https://github.com/CosmWasm/wasmvm/releases/download/v1.2.1/libwasmvm_muslc.x86_64.a -O /lib/libwasmvm_muslc.a
 
 git clone https://github.com/comdex-official/comdex.git
 cd comdex
-git checkout v9.1.2
+git checkout v11.5.0
 
 
 BUILD_TAGS=muslc LINK_STATICALLY=true make build
