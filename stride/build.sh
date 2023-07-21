8e26d868cf96df7484815f1058a13ca90b3cf282
NATIVEL Nicolas <nicolas@klub.ki>
2023-07-21 08:05:03 +0200

    Stride 12.1.0

--- stride/build.sh
+++ stride/build.sh
@@ -5,5 +5,5 @@ apk add git gcc build-base linux-headers
 git clone https://github.com/Stride-Labs/stride.git
 pwd
 cd stride
-git checkout v11.0.0
+git checkout v12.1.0
 BUILD_TAGS=muslc LINK_STATICALLY=true make build
